28af8af58e468e69e43f7b8c889c82f25803e778
mizugramming <ktbys0315@icloud.com>
2026-09-08 16:43:29 +0900

    feat: ルーム画面を寿司店モチーフに刷新

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:spajam2026/main.dart';
@@ -9,6 +10,26 @@ void main() {
 
     expect(find.text('会輪'), findsOneWidget);
     expect(find.text('部屋を作る'), findsOneWidget);
-    expect(find.text('部屋に入る'), findsOneWidget);
+    expect(find.text('参加する'), findsOneWidget);
+    expect(find.text('AI寿司大将'), findsNothing);
+  });
+
+  testWidgets('参加画面では部屋番号を大文字化して6文字を検証する', (tester) async {
+    await tester.pumpWidget(const AppRoot());
+    await tester.pumpAndSettle();
+
+    await tester.tap(find.text('参加する'));
+    await tester.pumpAndSettle();
+
+    final joinButtonFinder = find.widgetWithText(FilledButton, '部屋に参加する');
+    expect(tester.widget<FilledButton>(joinButtonFinder).onPressed, isNull);
+
+    await tester.enterText(find.byType(TextField), 'a7k3px');
+    await tester.pump();
+
+    final field = tester.widget<TextField>(find.byType(TextField));
+    expect(field.controller?.text, 'A7K3PX');
+    expect(find.text('6 / 6'), findsOneWidget);
+    expect(tester.widget<FilledButton>(joinButtonFinder).onPressed, isNotNull);
   });
 }
